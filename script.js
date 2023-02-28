45c45dacf1f23e13481e6ecebedf3b6ea2ce8d87
Alex Ni <125639818+bcrtrading@users.noreply.github.com>
2023-02-28 14:22:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,28 @@
 
   <button type="submit">Get Cost Estimate</button>
 </form>
+
+
+// Get references to the HTML elements
+const form = document.querySelector('#project-details-form');
+const nameInput = document.querySelector('#project-name');
+const typeInput = document.querySelector('#project-type');
+const sizeInput = document.querySelector('#project-size');
+const submitButton = document.querySelector('#submit-button');
+
+// Add an event listener to the form's submit button
+submitButton.addEventListener('click', function(event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the values from the input fields
+  const projectName = nameInput.value;
+  const projectType = typeInput.value;
+  const projectSize = parseFloat(sizeInput.value);
+
+  // Call the estimateProjectCost() function to calculate the project cost
+  const estimatedCost = estimateProjectCost(projectType, projectSize);
+
+  // Display the estimated cost to the user
+  alert(`The estimated cost for ${projectName} is $${estimatedCost}.`);
+});
