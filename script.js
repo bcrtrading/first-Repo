e1d245b701662cf36f0c2a13032013efc0641476
Alex Ni <125639818+bcrtrading@users.noreply.github.com>
2023-02-28 15:21:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,36 +6,31 @@
     <option value="industrial">Industrial</option>
   </select>
 
-  <label for="project-location">Project Location:</label>
-  <input type="text" id="project-location" placeholder="Enter project location">
-
-  <label for="project-timeline">Project Timeline (in months):</label>
-  <input type="number" id="project-timeline" placeholder="Enter project timeline in months">
+  <label for="project-size">Project Size:</label>
+  <input type="number" id="project-size" placeholder="Enter project size in square feet">
 
   <button type="submit">Get Cost Estimate</button>
 </form>
 
+<script>
+  // Get references to the HTML elements
+  const form = document.querySelector('#project-details-form');
+  const typeInput = document.querySelector('#project-type');
+  const sizeInput = document.querySelector('#project-size');
 
-// Get references to the HTML elements
-const form = document.querySelector('#project-details-form');
-const nameInput = document.querySelector('#project-name');
-const typeInput = document.querySelector('#project-type');
-const sizeInput = document.querySelector('#project-size');
-const submitButton = document.querySelector('#submit-button');
+  // Add an event listener to the form's submit button
+  form.addEventListener('submit', function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
-// Add an event listener to the form's submit button
-submitButton.addEventListener('click', function(event) {
-  // Prevent the default form submission behavior
-  event.preventDefault();
+    // Get the values from the input fields
+    const projectType = typeInput.value;
+    const projectSize = parseFloat(sizeInput.value);
 
-  // Get the values from the input fields
-  const projectName = nameInput.value;
-  const projectType = typeInput.value;
-  const projectSize = parseFloat(sizeInput.value);
+    // Call the estimateProjectCost() function to calculate the project cost
+    const estimatedCost = estimateProjectCost(projectType, projectSize);
 
-  // Call the estimateProjectCost() function to calculate the project cost
-  const estimatedCost = estimateProjectCost(projectType, projectSize);
-
-  // Display the estimated cost to the user
-  alert(`The estimated cost for ${projectName} is $${estimatedCost}.`);
-});
+    // Display the estimated cost to the user
+    alert(`The estimated cost is $${estimatedCost}.`);
+  });
+</script>
